1807a7b6b659ef43212a646705d3015cbe3e29e5
fengji1126 <13063939831@163.com>
2018-08-18 20:47:29 +0800

    ajax登录校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -15,31 +15,56 @@ $(function () {
             min: 2,
             max: 6,
             message: "用户名长度必须在2-6位"
+          },
+          callback:{
+            message: "用户名有误"            
           }
         }
       },
       password: {
-        validators:{
-          message:"密码不能为空"
-        },
-        stringLength:{
-          min:6,
-          max:12,
-          message:"密码长度必须在6-12位"
+        validators: {
+          notEmpty: {
+            message: "密码不能为空"
+          },
+          stringLength: {
+            min: 6,
+            max: 12,
+            message: "密码长度必须在6-12位"
+          },
+          callback:{
+            message: "密码有误"            
+          }
         }
       }
     }
+
   });
-
-
-
-
-
-
 });
 
-$("#form").on("success.form.bv",function () {
+$("#form").on("success.form.bv",function (e) {
   e.preventDefault();
   console.log("表单校验被阻止");
   
+  $.ajax({
+    type:"post",
+    url:"/employee/employeeLogin",
+    data:$("#form").serialize(),
+    dataType:"json",
+    success:function(info){
+      console.log(info);
+      if(info.success){
+        location.href = "index.html" 
+      }
+      if(info.error === 1000){
+        $("#form").data("bootstrapValidator").updateStatus("username","INVALID","callback")
+      }
+      if(info.error === 1001){
+        $("#form").data("bootstrapValidator").updateStatus("password","INVALID","callback")
+      }
+    }
+  })
+})
+
+$('[type="reset"]').click(function () {
+  $("#form").data("bootstrapValidator").resetForm()
 })
