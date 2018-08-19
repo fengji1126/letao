c83610285fa1d37973246fb48113409a48b08c18
fengji1126 <13063939831@163.com>
2018-08-19 20:49:44 +0800

    用户管理，分页渲染完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,19 +1,35 @@
 $(function () {
+  var currentPage = 1;
+  var pageSize = 5;
+
+  render();
+  function render() {
+    $.ajax({
+      type:"get",
+      url:"/user/queryUser",
+      data:{
+        page:currentPage,
+        pageSize:pageSize
+      },
+      dataType:"json",
+      success:function (info) {
+        console.log(info);
+        var htmlStar = template("tpl",info);
+        $("tbody").html(htmlStar);
   
-  $.ajax({
-    type:"get",
-    url:"/user/queryUser",
-    data:{
-      page:1,
-      pageSize:5
-    },
-    dataType:"json",
-    success:function (info) {
-      console.log(info);
-      var htmlStar = template("tpl",info);
-      $("tbody").html(htmlStar);
-    }
-  })
+        $("#paginator").bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          totalPages:Math.ceil(info.total/info.size ),
+          currentPage:info.page,
+          onPageClicked:function (a,b,c,page) {
+            currentPage = page;
+            render()
+          }
+        })
+      }
+    })
+  }
+
 
 
 
