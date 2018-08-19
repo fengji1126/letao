c85b493efca895bdea9c92f7716ba22d294ef223
fengji1126 <13063939831@163.com>
2018-08-19 21:42:14 +0800

    用户管理完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,6 +1,8 @@
 $(function () {
   var currentPage = 1;
   var pageSize = 5;
+  var currentId;
+  var isDelete;
 
   render();
   function render() {
@@ -31,7 +33,31 @@ $(function () {
   }
 
 
+  $("tbody").on("click",".btn",function() {
+    // console.log(1);
+    $("#userMotal").modal("show");
+    currentId = $(this).parent().data("id");
+    isDelete = $(this).hasClass("btn-danger")?0:1;
+    
+  })
 
+  $("#submitbtn").click(function () {
+    console.log(currentId);
+    console.log(isDelete);
+    $.ajax({
+      type:"post",
+      url:"/user/updateUser",
+      data:{
+        id:currentId,
+        isDelete:isDelete
+      },
+      success:function (info) {
+        $("#userMotal").modal("hide");
+        render();
+      }
+    })
+    
+  })
 
 
 })
